959736cddae730bf5440f6693df03bc5c504cd37
thencious <danielyin0920@hotmail.com>
2017-08-19 22:31:31 +0100

    finished books app

--- YoutubeApp/src/index.js
+++ YoutubeApp/src/index.js
@@ -1,9 +1,10 @@
+import _                    from 'lodash';
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
+import ReactDOM             from 'react-dom';
+import YTSearch             from 'youtube-api-search';
+import SearchBar            from './components/search_bar';
+import VideoList            from './components/video_list';
+import VideoDetail          from './components/video_detail';
 
 const API_KEY = 'AIzaSyAXRdgqnkTztiqAYagvo_0JqzH2mhyO_Pw';
 
@@ -17,19 +18,25 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
-       });
+      });
     });
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
       <div>
-        <SearchBar />
-        <VideoDetail videos ={this.state.selectedVideo} />
+        <SearchBar onSearchTermChange={ videoSearch } />
+        <VideoDetail video ={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
           videos={this.state.videos} />
